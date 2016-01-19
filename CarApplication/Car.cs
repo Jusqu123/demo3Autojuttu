8d5fa3403da791a0d1ce917d7160847c659e5561
Jusqu123 <jusquu123@gmail.com>
2016-01-19 10:12:09 +0200

    initial commit

---
+++ CarApplication/Car.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{
+    class Car
+    {
+        // fied member
+
+        //properties
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public double Engine { get; set; }
+        public int Speed { get; set; }
+        public bool FuzzyDices { get; set; }
+
+        // constructor
+        public Car()
+        {
+            FuzzyDices = true;
+        }
+
+
+        // parametric constructor
+        public Car(string model)
+        {
+            Model = model;
+        }
+
+        // method displays car data
+        public void PrintData()
+        {
+            Console.WriteLine("Car Data: ");
+            Console.WriteLine("- model : " + Model);
+            Console.WriteLine("- Color : " + Color);
+            Console.WriteLine("- engine : " + Engine);
+            Console.WriteLine("- speed : " + Speed);
+            Console.WriteLine("- fuzzydices : " + FuzzyDices);
+        }
+        // method gives more speed
+        public void Accelerate(int newSpeed)
+        {
+            Speed += newSpeed;
+        }
+    }
+}
---
+++ CarApplication/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApplication
+{// https://github.com/Jusqu123/demo3Autojuttu.git
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // create one car instance
+            Car car = new Car();
+            car.Model = "Audi";
+            car.Color = "Silver";
+            car.Engine = 2.0;
+            car.FuzzyDices = false;
+            car.Speed = 100;
+            car.PrintData();
+
+
+
+
+            Car nascar = new Car("Toyota");
+
+            //    Console.WriteLine("FuzzyDices = " + nascar.FuzzyDices);
+            //  Console.WriteLine("Model = " + nascar.Model);
+
+
+            nascar.Speed = 200;
+            nascar.Color = "Red";
+            nascar.Engine = 5.0;
+            nascar.FuzzyDices = true;
+            nascar.PrintData();
+            nascar.Accelerate(50);
+            nascar.PrintData();
+            Console.ReadLine();
+        }
+    }
+}
